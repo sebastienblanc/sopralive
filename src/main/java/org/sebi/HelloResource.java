ccc09eee633a401fa2efb01533f6b2075e384eef
sebastienblanc <scm.blanc@gmail.com>
2021-08-31 10:56:37 +0200

    add stuff

--- src/main/java/org/sebi/HelloResource.java
+++ src/main/java/org/sebi/HelloResource.java
@@ -33,6 +33,6 @@ public class HelloResource {
     @Path("/fr")
     @Produces(MediaType.TEXT_PLAIN)
     public String bonjour() {
-        return "Bonjour la Suisse!";
+        return "Bonjour la Suisse!!";
     }
 }
